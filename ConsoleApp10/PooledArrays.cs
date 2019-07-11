b3b4d7493290cdc8f9200e1e92dcd5a7ac80809b
Dax Fohl <dafohl@microsoft.com>
2019-07-10 21:09:55 -0700

    add class

---
+++ ConsoleApp10/PooledArrays.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PooledArrays
+{
+    public static class PooledArrayExtensions
+    {
+        public static PooledArray<U> SelectArray<T, U>(this IEnumerable<T> xs, Func<T, U> f)
+        {
+            if (xs is IReadOnlyList<T>)
+            {
+                return SelectArray((IReadOnlyList<T>)xs, f);
+            }
+
+            var array = ArrayPool<U>.Shared.Rent(0);
+            Console.WriteLine($"req: 0, got: {array.Length}");
+            int i = 0;
+            try
+            {
+                foreach (var x in xs)
+                {
+                    if (i >= array.Length)
+                    {
+                        var size = i == 0 ? 1 : i * 2;
+                        var newArray = ArrayPool<U>.Shared.Rent(size);
+                        Console.WriteLine($"req: {size}, got: {newArray.Length}");
+                        Array.Copy(array, 0, newArray, 0, i);
+                        ArrayPool<U>.Shared.Return(array);
+                        array = newArray;
+                    }
+
+                    array[i] = f(x);
+                    ++i;
+                }
+            }
+            catch
+            {
+                ArrayPool<U>.Shared.Return(array);
+                throw;
+            }
+
+            return new PooledArray<U>(array, i);
+        }
+
+        public static PooledArray<U> SelectArray<T, U>(this IReadOnlyList<T> xs, Func<T, U> f)
+        {
+            var array = ArrayPool<U>.Shared.Rent(xs.Count);
+            Console.WriteLine($"req: {xs.Count}, got: {array.Length}");
+            try
+            {
+                for (var i = 0; i < xs.Count; ++i)
+                {
+                    array[i] = f(xs[i]);
+                }
+            }
+            catch
+            {
+                ArrayPool<U>.Shared.Return(array);
+                throw;
+            }
+
+            return new PooledArray<U>(array, xs.Count);
+        }
+    }
+
+    public class PooledArray<T> : IDisposable, IReadOnlyList<T>
+    {
+        private readonly T[] array;
+        private readonly int length;
+        public PooledArray(T[] array, int length)
+        {
+            this.array = array;
+            this.length = length;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return array[index];
+            }
+        }
+
+        public int Count => length;
+
+        public void Dispose()
+        {
+            ArrayPool<T>.Shared.Return(array);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this.array, this.length);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator(this.array, this.length);
+        }
+
+        class Enumerator : IEnumerator<T>
+        {
+            private int i = -1;
+            private readonly T[] array;
+            private readonly int length;
+            public Enumerator(T[] array, int length)
+            {
+                this.array = array;
+                this.length = length;
+            }
+
+            public T Current => array[i];
+
+            object IEnumerator.Current => array[i];
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (i == length - 1) return false;
+                ++i;
+                return true;
+            }
+
+            public void Reset()
+            {
+                this.i = 0;
+            }
+        }
+    }
+}
--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,133 +1,55 @@
 ﻿using System;
+using System.Buffers;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Remoting.Channels;
 using System.Threading;
-using System.Threading.Tasks;
+using PooledArrays;
 
 namespace ConsoleApp10
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine($"GCSettings.IsServerGC = {System.Runtime.GCSettings.IsServerGC}");
-			Console.WriteLine($"GCSettings.LatencyMode = {System.Runtime.GCSettings.LatencyMode}");
-			Console.WriteLine($"GCSettings.LargeObjectHeapCompactionMode = {System.Runtime.GCSettings.LargeObjectHeapCompactionMode}");
-			new Thread(() =>
-			{
-				var timer = new Stopwatch();
-				while (true)
-				{
-					timer.Restart();
-					Thread.Sleep(1);
-					timer.Stop();
-					// allow a little bit of leeway
-					if (timer.ElapsedMilliseconds > 2)
-					{
-						// Record the pause
-						Console.WriteLine($"Pause {timer.ElapsedMilliseconds}");
-					}
-				}
-			}).Start();
-			var random = new Random();
-			var start = 0;
-			var end = 256;
-			var maxLen = 2000;
-			var stringCache = new LRUCache<int, string>(20000);
-			var bytesCache = new LRUCache<int, byte[]>(20000);
-			//var bytes2Cache = new LRUCache<int, byte[]>(10000);
-			for (var i = 0; i < 5; ++i)
-			{
-				new Thread(() =>
-				{
-					var threadCounter = 0;
-					while (true)
-					{
-						var text = new string((char)random.Next(start, end + 1), random.Next(maxLen));
-						stringCache.Set(text.GetHashCode(), text);
+        static void Main(string[] args)
+        {
+            using (var sb = new LinqSandbox())
+            {
+                var bs = Enumerable.Range(0, 10000000);
+                var cs = sb.SelectArray(bs, i => i + 1);
+                var ds = sb.SelectArray(cs, i => i + 1);
+                var es = sb.SelectArray(ds, i => i + 1);
+                var fs = sb.SelectArray(es, i => i + 1);
+                var gs = sb.SelectArray(fs, i => i + 1);
+                var hs = sb.SelectArray(gs, i => i + 1);
+                var ks = sb.SelectArray(hs, i => i + 1);
+                Console.WriteLine(es.Max());
+                Console.WriteLine(es.Max());
+                Console.WriteLine(es.Max());
+            }
 
-						var bytes = new byte[80 * 1024];
-						random.NextBytes(bytes);
-						bytesCache.Set(bytes.GetHashCode(), bytes);
-
-						var bytes2 = new byte[90 * 1024];
-						//random.NextBytes(bytes2);
-						//bytes2Cache.Set(bytes2.GetHashCode(), bytes2);
-
-						threadCounter++;
-						Thread.Sleep(1); // So we don't thrash the CPU!!!!
-					}
-				}).Start();
-			}
-
-			Console.ReadKey();
-		}
-	}
-	public class LRUCache<K, V>
-	{
-		private int capacity;
-		private Dictionary<K, LinkedListNode<LRUCacheItem<K, V>>> cacheMap = new Dictionary<K, LinkedListNode<LRUCacheItem<K, V>>>();
-		private LinkedList<LRUCacheItem<K, V>> lruList = new LinkedList<LRUCacheItem<K, V>>();
-
-		public LRUCache(int capacity)
-		{
-			this.capacity = capacity;
-		}
-
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public V Get(K key)
-		{
-			LinkedListNode<LRUCacheItem<K, V>> node;
-			if (cacheMap.TryGetValue(key, out node))
-			{
-				V value = node.Value.value;
-				lruList.Remove(node);
-				lruList.AddLast(node);
-				return value;
-			}
-			return default(V);
-		}
-
-		[MethodImpl(MethodImplOptions.Synchronized)]
-		public void Set(K key, V val)
-		{
-			if (cacheMap.Count >= capacity)
-			{
-				RemoveFirst();
-			}
-
-			LRUCacheItem<K, V> cacheItem = new LRUCacheItem<K, V>(key, val);
-			LinkedListNode<LRUCacheItem<K, V>> node = new LinkedListNode<LRUCacheItem<K, V>>(cacheItem);
-			lruList.AddLast(node);
-			try
-			{
-				cacheMap.Add(key, node);
-			}
-			catch { }
-		}
-
-		private void RemoveFirst()
-		{
-			// Remove from LRUPriority
-			LinkedListNode<LRUCacheItem<K, V>> node = lruList.First;
-			lruList.RemoveFirst();
-
-			// Remove from cache
-			cacheMap.Remove(node.Value.key);
-		}
+            Console.ReadKey();
+        }
 	}
 
-	class LRUCacheItem<K, V>
-	{
-		public LRUCacheItem(K k, V v)
-		{
-			key = k;
-			value = v;
-		}
-		public K key;
-		public V value;
-	}
+    class LinqSandbox : IDisposable
+    {
+        List<IDisposable> pooledArrays = new List<IDisposable>();
+
+        public PooledArray<U> SelectArray<T, U>(IEnumerable<T> xs, Func<T, U> f)
+        {
+            var array = xs.SelectArray(f);
+            pooledArrays.Add(array);
+            return array;
+        }
+
+        public void Dispose()
+        {
+            foreach (var array in pooledArrays)
+            {
+                array.Dispose();
+            }
+        }
+    }
+
 }
